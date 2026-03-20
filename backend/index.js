0b6615a63823897882005fe3cc651733c9c8d74f
Atharrv2459 <atharrv@gmail.com>
2026-03-21 00:52:04 +0530

    Added email feature using nodemailer. Added admin api of creating a user. Added some customer apis (creating a ticket and viewing your tickets)

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import { testDbConnection } from './src/config/db.js';
+import adminRoutes from './src/routes/admin_routes.js';
+import { bootstrapAdmin } from './src/config/bootstrapAdmin.js';
+import customerRoutes from './src/routes/customer_routes.js';
+
 
 const app = express();
 
@@ -11,6 +15,12 @@ app.use(cors());
 app.use(express.json());
 
 await testDbConnection();
+await bootstrapAdmin();
+
+app.use('/api/admin', adminRoutes);
+app.use('/api/customer', customerRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
